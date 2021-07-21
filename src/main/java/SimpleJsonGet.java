ecd6fcdd0dc01729c6e2517b1c53ae4f16763340
mivchal <mivchal@hotmail.com>
2021-07-21 23:57:58 +0200

    test commit 1

--- src/main/java/SimpleJsonGet.java
+++ src/main/java/SimpleJsonGet.java
@@ -4,14 +4,15 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URL;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 
 public class SimpleJsonGet {
+
     public static void main(String[] args) throws IOException {
         String apiURL = "https://restcountries.eu/rest/v2/name/poland";
 
-        String jsonString = IOUtils.toString(new URL(apiURL), Charset.forName("UTF-8"));
+        String jsonString = IOUtils.toString(new URL(apiURL), StandardCharsets.UTF_8);
         jsonString = jsonString.substring(1, jsonString.length() - 1);
 
         JSONObject obj = new JSONObject(jsonString);
